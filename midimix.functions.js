380e89d9d479cd967f238a84e7ae6cdbc491897e
dcupmusic <dcupmusic@gmail.com>
2016-09-17 21:08:17 +1000

    FB

--- midimix.functions.js
+++ midimix.functions.js
@@ -29,42 +29,16 @@ var parameters = {
 			cursorDevice.getMacro(data1 - (this.offset +32)).set(data2,128);
 		}
 	},
-	pageScroll: function(data1, data2)
- 	{
- 		if(data1 == 33 && data2 != 0){
- 			cursorDevice.setParameterPage(0);
- 		}else if(data1 ==34 && data2 != 0){
- 			cursorDevice.setParameterPage(1);
- 		}else if(data1 ==35 && data2 != 0){
- 			cursorDevice.setParameterPage(2);
- 		}else if(data1 ==36 && data2 != 0){
- 			cursorDevice.setParameterPage(3);
- 		}else if(data1 ==37 && data2 != 0){
- 			cursorDevice.setParameterPage(4);
- 		}else if(data1 ==38 && data2 != 0){
- 			cursorDevice.setParameterPage(5);
- 		}else if(data1 ==39 && data2 != 0){
- 			cursorDevice.setParameterPage(6);
- 		}else if(data1 ==40 && data2 != 0){
- 			cursorDevice.setParameterPage(7);
- 		}else if(data1 ==49 && data2 != 0){
- 			cursorDevice.setParameterPage(8);
- 		}else if(data1 ==50 && data2 != 0){
- 			cursorDevice.setParameterPage(9);
- 		}else if(data1 ==51 && data2 != 0){
- 			cursorDevice.setParameterPage(10);
- 		}else if(data1 ==52 && data2 != 0){
- 			cursorDevice.setParameterPage(11);
- 		}else if(data1 ==53 && data2 != 0){
- 			cursorDevice.setParameterPage(12);
- 		}else if(data1 ==54 && data2 != 0){
- 			cursorDevice.setParameterPage(13);
- 		}else if(data1 ==55 && data2 != 0){
- 			cursorDevice.setParameterPage(14);
- 		}else if(data1 ==56 && data2 != 0){
- 			cursorDevice.setParameterPage(15);
- 		}
- 	},
+	pageScroll: function(data1, data2) {
+if (data2 != 0) {
+if (data1 >= 33 && data1 <= 39)
+cursorDevice.setParameterPage(data1 - 33);
+if (data1 === 40)
+cursorDevice.setParameterPage(7);
+if (data1 >= 49 && data1 <= 56)
+cursorDevice.setParameterPage(data1 - 41);
+}
+},
 
  	update: function()
  	{

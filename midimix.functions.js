cad45b87a3919b221b0221f4b02d807da5e321b5
dcupmusic <dcupmusic@gmail.com>
2016-09-17 20:43:25 +1000

    re macro

--- midimix.functions.js
+++ midimix.functions.js
@@ -23,12 +23,7 @@ var parameters = {
 		}
 	},
 
-	macro: function(data1, data2)
-	{
-		if(data1 >= this.offset +32 && data1 < this.offset 40){
-			cursorDevice.getMacro(data1 - (this.offset +32)).set(data2,128);
-		}
-	},
+
 
 
 	pageScroll: function(data1, data2)
